43402094296f5a6018fe5695674d6ef66b2b7b7d
Andrew <Asaganda@gmail.com>
2017-04-26 15:36:23 -0400

    Initial commit - landing page w/ responsive

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("back_arrow").onclick = function(e) { 
+  e.preventDefault();
+  alert("You Clicked A Button"); 
+};
+
+document.getElementById("backarrow").onclick = function(e) {
+  e.preventDefault();
+  alert("You Clicked A button");
+}
+
+document.getElementById("share").onclick = function(e) {
+  e.preventDefault();
+  alert("You Clicked A button");
+}
